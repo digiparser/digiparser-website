4fa85e8f0600b8bfa68102717c724d1ff1e4029a
Pankaj Patidar <pankaj9296@gmail.com>
2024-03-04 19:10:29 +0530

    .

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -9,16 +9,19 @@ import Image from "next/image";
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
+      <div className={'flex flex-1 items-center mt-10'}>
+        <div className={'flex flex-1 justify-center pl-5'}>
+          <Image src="/img/logo.svg" alt="logo" width={100} height={100} className={'mr-1 dark:hidden'}/>
+          <Image src="/img/logo-light.svg" alt="logo" width={100} height={100} className={'mr-1 hidden dark:block'}/>
+        </div>
+      </div>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-1 flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <div>
-            <Image src="/img/logo.svg" alt="logo" width={16} height={16} className={'mr-1 dark:hidden'}/>
-            <Image src="/img/logo-light.svg" alt="logo" width={16} height={16} className={'mr-1 hidden dark:block'}/>
-          </div>
+        <div className="flex flex-1 flex-col items-center gap-4 px-8 md:gap-2 md:px-0">
           <span className="hidden font-bold sm:inline-block">
             {siteConfig.name}
           </span>
-          <div className={'text-xs ml-auto text-muted-foreground'}>Copyright © 2023-{new Date().getFullYear()} {siteConfig.name}</div>
+          <div className={'text-xs text-muted-foreground'}>Copyright ©
+            2023-{new Date().getFullYear()} {siteConfig.name}</div>
         </div>
       </div>
     </footer>
